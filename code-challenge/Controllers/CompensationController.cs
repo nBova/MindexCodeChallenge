3bdd37d42f80f85b65ba466dd3b96bcfc54b893e
Nick Bova <npb7642@rit.edu>
2021-05-20 00:24:45 -0400

    implemented apis

--- code-challenge/Controllers/CompensationController.cs
+++ code-challenge/Controllers/CompensationController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using challenge.Services;
 using challenge.Models;
+using System;
 
 namespace challenge.Controllers
 {
@@ -11,6 +12,29 @@ namespace challenge.Controllers
     public class CompensationController : Controller
     {
         private readonly ILogger _logger;
-        private readonly IEmployeeService _employeeService;
+        private readonly ICompensationService _compensationService;
+
+        [HttpPost]
+        public IActionResult CreateCompensation([FromBody] Compensation compensation)
+        {
+            _logger.LogDebug($"Received compensation create request for Employee with id: '{compensation.employee.EmployeeId}'");
+
+            _compensationService.AddCompensation(compensation);
+
+            return CreatedAtRoute("getCompensationById", new { id = compensation.employee.EmployeeId }, compensation);
+        }
+
+        [HttpGet("{id}", Name = "getCompensationById")]
+        public IActionResult GetCompensationById(String id)
+        {
+            _logger.LogDebug($"Received compensation get request for Employee with id: '{id}'");
+
+            var compensation = _compensationService.GetById(id);
+
+            if (compensation == null)
+                return NotFound();
+
+            return Ok(compensation);
+        }
     }
 }
